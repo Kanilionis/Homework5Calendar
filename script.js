7278fe51549ecd4bda88751b695283e70423a04e
Kanilionis <kanilionis@gmail.com>
2020-10-06 09:19:02 -0600

    added clear schedule button, changed hours to id=hourofday, if/then statement so colors change according to time

--- script.js
+++ script.js
@@ -1,71 +1,67 @@
-    $(document).ready(function(){
+    
+  $(document).ready(function(){
 
-   var currentDay = document.querySelector("#currentDay")
-   var saveBtn = document.getElementsByClassName("saveBtn");
-   var descriptionInput = document.getElementsByClassName("description");
-  //  var timeOfDay = document.querySelector(".hour");
-  //  var newTask = $(".description").val();
-   var today = dayjs().format("MMMM, D, YYYY");
-   var currentTime = dayjs().format();
-   console.log(currentTime);
-   currentDay.textContent = today;
-   var timeBlock = document.querySelector(".hour");
-        timeBlock = dayjs().set("hour", 09);
-        console.log(timeBlock);
+  var today = dayjs().format("MMMM, D, YYYY");
+  var currentTime = dayjs().format();
 
-    $(".saveBtn").click(function(e){
-        e.preventDefault();
-        if(descriptionInput !== null){
-        console.log(descriptionInput.value);
-            } else{
-            }
-            localStorage.setItem("tasks", descriptionInput.value);
-            })
+  console.log(currentTime);
+  $("#currentDay").text(today);
 
-function saveData(){
-
-}
-// var startTime = dayjs().startOf("day");
-
-// function timeBlockColors(){
-//     if(dayjs().isBefore)
-// }
-// var isAfter = currentTime.isAfter()
-// var hours = ["9", "10", "11", "12", "1", "2", "3", "4", "5"];
-// var getHours = dayjs().startOf("hour")
-
-// function startCalendar(e){
-//     e.preventDefault;
-// }
-
-var militaryHours = ["9", "10", "11", "12", "13", "14", "15", "16", "17"]
-function timeBlockColors(){
-    var militaryTime = dayjs().get("hour");
-    console.log(militaryTime);
-    for (var i=0; i<militaryHours.length; i++){
-        if(militaryTime < militaryHours[i]){
-            $(".description").attr(".future");
-        } else if (militaryTime === militaryHours[i]){
-            $(".description").attr(".present");
-        } else if (militaryTime > militaryHours[i]){
-            $(".description").attr(".past");
-        }
-        }
-    };
-
-
+// do this for each row/time - change to 10, 11, etc - for 1:00 make the id 13
+  $("#9 .description").val(localStorage.getItem("9"));
+  $("#10 .description").val(localStorage.getItem("10"));
+  $("#11 .description").val(localStorage.getItem("11"));
+  $("#12 .description").val(localStorage.getItem("12"));
+  $("#13 .description").val(localStorage.getItem("13"));
+  $("#14 .description").val(localStorage.getItem("14"));
+  $("#15 .description").val(localStorage.getItem("15"));
+  $("#16 .description").val(localStorage.getItem("16"));
+  $("#17 .description").val(localStorage.getItem("17"));
   
-  
+  $(".clear-cal").on("click", function(){
+    var textArea = $("#9 #10 #11 #12 #13 #14 #15 #16 #17.description").val();
+    console.log(textArea);
+    localStorage.clear();
+    location.reload();
+    });
 
-//   var taskStr = JSON.stringify(descriptionInput);
-
-//   function saveDescription(){
-//   var descriptionSaved = taskStr[0];
-//   descriptionInput.addAttribute("class", "hidden");
-//   descriptionSaved.removeAttribute("class", "hidden");
-//   localStorage.getItem("tasks", descriptionInput.value)
-//   console.log(taskStr);
-
-//   }
-//   var savedTasks = localStorage.getItem("tasks");
+  $(".saveBtn").on("click", function(){
+    // e.preventDefault();
+    var inputVal = $(this).siblings(".description").val();
+    var id = $(this).parent().attr("id");
+    // key = id, value = inputVal
+        localStorage.setItem(id, inputVal);
+        console.log(inputVal, id);
     })
+
+  function colorChange(){
+    // grabbing specific id
+    var currentHour = moment().hours();
+      console.log(currentHour);
+    $(".time-block").each(function(){
+      var timeBlocks = $(this).attr("id");
+      console.log(timeBlocks);
+          if(timeBlocks<currentHour){
+              $(this).addClass("past");
+          } else if(timeBlocks>currentHour){
+              $(this).addClass("future");
+          } else{
+              $(this).addClass("present")
+            }
+            })
+            }
+colorChange()
+
+
+})
+ 
+
+
+      
+
+
+
+    
+
+  
+
